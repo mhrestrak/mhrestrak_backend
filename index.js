a426156e9aa8a991867ccec95e8fe9287fae446b
josh-code <fernandes.developer@gmail.com>
2021-07-29 17:26:33 +0530

    First commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+
+require("./startup/routes")(app);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ startup/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const users = require("../routes/users");
+const cors = require("cors");
+// const auth = require("../routes/auth");
+// const error = require("../middleware/error");
+
+module.exports = function (app) {
+  //----------------------Setting route handlers--------------------------
+  app.use(express.json());
+  app.use("/api/users", users);
+  //   app.use(cors({ origin: "https://hidden-beyond-42098.herokuapp.com" }));
+  //   app.use("/api/auth", auth);
+  //--------------------Request pipeline Error handeling---------------------
+  //   app.use(error);
+};
