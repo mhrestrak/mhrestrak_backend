02a3c9b2f47034336733811449d0b4282dd6cbc2
mhrestrak@gmail.com <fernandes.developer@gmail.com>
2021-08-26 13:22:01 +0530

    added updates

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,7 @@ const users = require("../routes/users");
 const cors = require("cors");
 const auth = require("../routes/auth");
 const resident = require("../routes/resident");
+const lists = require("../routes/lists");
 const test = require("../routes/test");
 // const error = require("../middleware/error");
 
@@ -14,6 +15,7 @@ module.exports = function (app) {
   app.use("/api/auth", auth);
   app.use("/api/test", test);
   app.use("/api/resident", resident);
+  app.use("/api/lists", lists);
   //--------------------Request pipeline Error handeling---------------------
   //   app.use(error);
 };
