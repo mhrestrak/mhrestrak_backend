bd7ca6a490fd58134ad8a75b01bd6ecc99f06200
Joshua Ferndes <fernandes.developer@gmail.com>
2023-01-03 14:29:46 +0530

    Added Static IP Config

--- startup/database.js
+++ startup/database.js
@@ -1,5 +1,6 @@
 const sql = require("mssql");
 const config = require("config");
+const HttpProxyAgent = require('http-proxy-agent');
 
 var DbConfig = {
   server: config.get("dbServer"),
@@ -10,8 +11,21 @@ var DbConfig = {
 
 module.exports = async function () {
   //-----------------------connecting mongo db---------------------------
+  console.log(1, process.env.QUOTAGUARDSTATIC_URL)
+  const agent = new HttpProxyAgent(process.env.QUOTAGUARDSTATIC_URL);
+
   try {
-    let pool = await sql.connect(DbConfig);
+    let pool = new sql.ConnectionPool({
+      server: config.get("dbServer"),
+      user: config.get("dbUser"),
+      password: config.get("dbPassword"),
+      database: config.get("db"),
+      options : {
+        encrypt : true,
+        agent
+      }
+    });
+    pool = await pool.connect()
     console.log("Connection established.");
     return pool;
   } catch (err) {
