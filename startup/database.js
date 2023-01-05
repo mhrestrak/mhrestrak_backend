1503db26a0ca55609df7c140da6610e271aa4c7d
Joshua Ferndes <fernandes.developer@gmail.com>
2023-01-05 11:34:06 +0530

    Proxy update

--- startup/database.js
+++ startup/database.js
@@ -1,17 +1,27 @@
 const sql = require("mssql");
+const HttpProxyAgent = require('http-proxy-agent');
 const config = require("config");
+
+
+const ProxyUrl = config.get("FIXIE_URL")
+const agent = new HttpProxyAgent(ProxyUrl);
 
 var DbConfig = {
   server: config.get("dbServer"),
   user: config.get("dbUser"),
   password: config.get("dbPassword"),
   database: config.get("db"),
+  options: {
+    encrypt: true,
+    agent: agent
+  }
 };
 
 module.exports = async function () {
   //-----------------------connecting mongo db---------------------------
   try {
-    let pool = await sql.connect(DbConfig);
+    const pool = new sql.ConnectionPool(DbConfig)
+    await pool.connect();
     console.log("Connection established.");
     return pool;
   } catch (err) {
@@ -19,8 +29,4 @@ module.exports = async function () {
     console.log(err);
     return { error: err.message };
   }
-
-  //   pool.request().query("SELECT * from ResProfile");
-  //   console.log(products);
 };
-// const winston = require("winston");
